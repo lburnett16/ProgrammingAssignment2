9f6446909d96b5d2bb232c612bca438b348f2317
lburnett16 <leah.bb0516@gmail.com>
2025-04-01 20:17:34 -0600

    Commit

--- Programming Assignment 2.R
+++ Programming Assignment 2.R
@@ -1,22 +1,17 @@
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL  # This will hold the inverse of the matrix
+  inv <- NULL  
   
-  # Function to set the matrix
   set <- function(y) {
     x <<- y
-    inv <<- NULL  # Reset the inverse when the matrix is changed
+    inv <<- NULL  
   }
   
-  # Function to get the matrix
   get <- function() x
   
-  # Function to set the inverse
   setinverse <- function(inverse) inv <<- inverse
-  
-  # Function to get the inverse
+ 
   getinverse <- function() inv
-  
-  # Return a list of the above functions
+ 
   list(set = set,
        get = get,
        setinverse = setinverse,
@@ -25,16 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x) {
   inv <- x$getinverse()
   
-  # If inverse is already cached, return it
+  
   if (!is.null(inv)) {
     message("Getting cached inverse")
     return(inv)
   }
-  
-  # If not cached, calculate the inverse
+ 
   mat <- x$get()
-  inv <- solve(mat)  # solve() computes the inverse
-  x$setinverse(inv)       # Store it in the cache
+  inv <- solve(mat)  
+  x$setinverse(inv)       
   inv
 }
 
